50c62688c9e831db21b644d5d68846d90f7ab37b
ChenfromChina123 <3301767269@qq.com>
2025-12-18 20:16:36 +0800

    Enhance Vocabulary module with Enterprise UI and Sidebar layout, move AI generation to separate view

--- aispring/src/main/java/com/aispring/entity/VocabularyWord.java
+++ aispring/src/main/java/com/aispring/entity/VocabularyWord.java
@@ -1,6 +1,7 @@
 package com.aispring.entity;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
