162bae68381a1c5b9be8401ce8ae22bd945755ca
ChenfromChina123 <3301767269@qq.com>
2026-01-05 15:40:48 +0800

    修复找回密码参数映射与错误响应格式

--- aispring/src/main/java/com/aispring/dto/request/ResetPasswordRequest.java
+++ aispring/src/main/java/com/aispring/dto/request/ResetPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.aispring.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,13 +11,16 @@ public class ResetPasswordRequest {
     
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")
+    @JsonProperty("email")
     private String email;
     
     @NotBlank(message = "验证码不能为空")
+    @JsonProperty("code")
     private String code;
     
     @NotBlank(message = "新密码不能为空")
     @Size(min = 6, message = "密码至少6位")
+    @JsonProperty("newPassword")
     private String newPassword;
 }
 
